d55cc04477481c8b49acaee781e1090b120b98c7
chicha099 <tdsserafin@gmail.com>
2022-02-12 00:11:16 -0300

    lkkl

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ import * as dat from "dat.gui";
 
 const textureLoader = new THREE.TextureLoader();
 
-const normalTexture = textureLoader.load("/textures/NormalMap.png");
+const normalTexture = textureLoader.load("https://i.ibb.co/Kss6kdc/Normal-Map.png");
 
 // Debug
 const gui = new dat.GUI();
